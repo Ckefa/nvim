87f486773d2c87f185ab73963bcdfe0b1417ad88
root <root@vmi2073828.contaboserver.net>
2024-11-09 10:16:32 +0100

    adding big daddy

---
+++ bigdaddy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+sudo apt install\
+	tmux\
+	net-tools \
+	git gcc \
+	ripgrep \
+	zoxide \
+	python3-venv\
+
+echo "================All Tools installed Success===============";
+
