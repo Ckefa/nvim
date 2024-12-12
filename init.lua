45b9e9fd010f0b617af847fb36df56379708ed56
kefa <ckefa65@gmail.com>
2024-12-12 16:09:06 +0300

    adding highlight on yank

--- init.lua
+++ init.lua
@@ -15,6 +15,17 @@ vim.g.mapleader = " "
 --   cache_enabled = 0,
 -- }
 
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("HighlightYank", {}),
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch", -- Highlight group to use
+      timeout = 200,         -- Duration of highlight in milliseconds
+    })
+  end,
+})
+
 -- Bootstrap lazy.nvim and all plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
