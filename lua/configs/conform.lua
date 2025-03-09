c8cc174d0d6369084e2c8098cc09a505cf3b2a5b
ckefa <ckefa65@gmail.com>
2025-03-09 22:14:38 +0300

    updating confirm.nvim autoformat file on save

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,18 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
-
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		css = { "prettier" },
+		html = { "prettier" },
+		go = { "gofmt" },
+		javascript = { "prettier" },
+		python = { "black" },
+		-- Add other languages as needed
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+	timeout_ms = 500,
+         		lsp_fallback = true,
+	},
 }
 
 return options
